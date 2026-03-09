5f499931c39f2b477a4f947dcf9aabe98ccb84c1
Leo Schlosser <leoschlosser@tutamail.com>
2026-03-09 19:41:50 +0100

    [asmjit] Bump to 2025-10-13.

--- ports/asmjit/portfile.cmake
+++ ports/asmjit/portfile.cmake
@@ -1,31 +1,30 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO asmjit/asmjit
-    REF fb9f82cb61df36aa513d054e748dc6769045f33e
-    SHA512 937a1ea1855d7eef53e6afc3401dd015e5de26d174dc667f8be4580a2d8388a703fff4298e4e2ca9ea490c5053197f33d3b010456985243931a1c0a6845926cc
+    REF 9eb6edbf711ceb25346ee40bae68b40a4505cdf5 # commited on 2025-05-08
+    SHA512 9c3567634205bf0c64462833b28b6bbf1c1965216aa09872196048953417fab18822d94d70d112f7a506396a5b94716d714316dd2559c3509edf1273650768b9
     HEAD_REF master
-    PATCHES
-        asmjit.patch
 )
 
-set(VCPKG_LIBRARY_LINKAGE static)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ASMJIT_STATIC)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DASMJIT_STATIC=TRUE
+        -DASMJIT_STATIC=${ASMJIT_STATIC}
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/asmjit)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/asmjit/core/api-config.h"
-#     "#if !defined(ASMJIT_STATIC)"
-#     "#if 0"
-# )
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/asmjit/core/api-config.h"
+        "#if !defined(ASMJIT_STATIC)"
+        "#if 0"
+    )
+endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
